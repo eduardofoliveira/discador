57a12d7973de66dc2451599c8cab365940debc5a
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-17 15:14:40 -0300

    Duas chamadas

--- fs-connector.js
+++ fs-connector.js
@@ -5,10 +5,21 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
 
   let from = '5511961197559'
 
-  conn.api(
-    `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},origination_caller_id_name=${from},sip_contact_user=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880115@54.233.223.179 35880115 XML discador`,
-    result => {
-      console.log(result)
-    }
-  );
+  setTimeout(() => {
+    conn.api(
+      `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},origination_caller_id_name=${from},sip_contact_user=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880115@54.233.223.179 35880115 XML discador`,
+      result => {
+        console.log(result)
+      }
+    );
+  }, 5000)
+
+  setTimeout(() => {
+    conn.api(
+      `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},origination_caller_id_name=${from},sip_contact_user=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880115@54.233.223.179 35880115 XML discador`,
+      result => {
+        console.log(result)
+      }
+    );
+  }, 10000)
 })
