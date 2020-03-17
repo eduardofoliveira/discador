61f2d9cf77bfbb4a7bb25b96272c528410157911
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-17 14:12:56 -0300

    first commit

---
+++ fs-connector.js
@@ -0,0 +1,12 @@
+const esl = require("modesl");
+
+conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
+  conn.events("json", "all");
+
+  conn.api(
+    `originate {origination_caller_id_number=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880115@54.233.223.179 35880115 XML discador`,
+    result => {
+      console.log(result)
+    }
+  );
+})
