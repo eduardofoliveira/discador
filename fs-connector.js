0da78c9ea2f9687793ead20a4a009085f58b59ab
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-17 15:05:05 -0300

    alterando

--- fs-connector.js
+++ fs-connector.js
@@ -6,7 +6,7 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   let from = '5511961197559'
 
   conn.api(
-    `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880866@54.233.223.179 35880115 XML discador`,
+    `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},origination_caller_id_name=${from},sip_contact_user=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880866@54.233.223.179 35880115 XML discador`,
     result => {
       console.log(result)
     }
