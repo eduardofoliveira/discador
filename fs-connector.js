50aa207876f1f37c21e933e8c0952982783bd969
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-17 14:30:35 -0300

    Alterando from

--- fs-connector.js
+++ fs-connector.js
@@ -3,7 +3,7 @@ const esl = require("modesl");
 conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   conn.events("json", "all");
 
-  let from = '551100000001'
+  let from = '5511961197559'
 
   conn.api(
     `originate {absolute_codec_string=^^:PCMU:PCMA,origination_caller_id_number=${from},bridge_generate_comfort_noise=true}sofia/gateway/ASTPP/551135880115@54.233.223.179 35880115 XML discador`,
