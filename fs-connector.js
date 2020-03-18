7eb9194e3578e06def184b5000219921981f942d
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-18 14:09:30 -0300

    Add Originate com Quantidade

--- fs-connector.js
+++ fs-connector.js
@@ -60,7 +60,7 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
         }
       }
     );
-    from = from++
+    from = (parseInt(from) + 1).toString()
   })
 
   em.on('showcalls', res => {
